2bded1b38743a775550f42f7c9722c5de4793891
Jian Huang <jihuang@adobe.com>
2023-05-02 10:57:47 -0400

    more styling

--- blocks/teasers/teasers.js
+++ blocks/teasers/teasers.js
@@ -21,26 +21,33 @@ export default async function decorate(block) {
     var decorativeWrapperElement = document.createElement('div');
     decorativeWrapperElement.classList.add('border', 'border-dark', 'p-3', 'bg-white');
 
+    const titleButton = row.querySelector('.button-container .button');
+    if(titleButton) {
+      titleButton.classList.add('btn', 'btn-secondary', 'btn-lg');
+
+      // every 3rd button
+      if((index +2) % 3 === 0) {
+        titleButton.classList.add('btn-sand');
+      }
+    }
+
+    const linkButton = row.querySelector('.button-container:last-child .button');
+    if(linkButton) {
+      linkButton.classList.add('btn', 'btn-dark');
+
+      linkButton.parentElement.classList.add('d-grid', 'justify-content-md-end');
+    }
+
     // if 2nd child element has content, then this is full content
     if(row.children[1].innerHTML) {
       row.classList.add('row');
 
       row.children[0].classList.add('col-12', 'col-md-8');
       row.children[1].classList.add('col-12', 'col-md-4');
+
+      titleButton.classList.add('btn-info');
     } else {
       colElement.classList.add('col-md-6');
-    }
-
-    var titleButton = row.querySelector('.button-container .button');
-    if(titleButton) {
-      titleButton.classList.add('btn', 'btn-secondary', 'btn-lg');
-    }
-
-    var linkButton = row.querySelector('.button-container:last-child .button');
-    if(linkButton) {
-      linkButton.classList.add('btn', 'btn-dark');
-
-      linkButton.parentElement.classList.add('d-grid', 'justify-content-md-end');
     }
 
     decorativeWrapperElement.append(row);
